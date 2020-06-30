31f52788640f39c3912f79d2f3ea367e6e4a6e92
Ryuya-Sasagawa <61501141+Ryuya-Sasagawa@users.noreply.github.com>
2020-06-30 15:31:53 +0900

    intver

--- InputNickName.h
+++ InputNickName.h
@@ -1,7 +1,46 @@
-ï»¿#pragma once
-typedef struct {
-	int i;
-}InputNickName_t;
-int InitializeInputNickName(InputNickName_t*);
-int UpdateInputNickName(InputNickName_t*);
-void FinalizeInputNickName(InputNickName_t*);
+#ifndef INPUTNICKNAME_H_
+#define INPUTNICKNAME_H_
+
+
+#include <iostream>
+#include <string>
+
+//–{”Ô—p
+//#include "SceneMgr.h"
+
+//ƒeƒXƒg—p
+#include "MouseState.h"
+#include "KeyboardState.h"
+
+
+//ƒeƒXƒg—p
+#define SCR_SIZE_X 1080
+#define SCR_SIZE_Y 720
+
+
+#define FONT_SIZE 16
+#define NAME_MAX 20
+
+enum InputNickNamePhase {standard, textBox, check, used};
+
+typedef struct InputNickName_t {
+	enum InputNickNamePhase phase = standard;
+	std::string nickName;
+} InputNickName_t;
+
+//–{”Ô—p
+int InitializeInputNickName(InputNickName_t *);
+int UpdateInputNickName(InputNickName_t *, Mouse_t *);
+void FinalizeInputNickName(InputNickName_t *);
+
+
+int click(Mouse_t *, int, int, int, int);
+int checkNickName(std::string);
+
+/*ƒeƒXƒg—p
+int test_Initialize(InputNickName_t *);
+int test_Update(InputNickName_t *, Mouse_t *);
+void test_Finelize(InputNickName_t *);
+*/
+
+#endif /*INPUTNICKNAME_H_*/
